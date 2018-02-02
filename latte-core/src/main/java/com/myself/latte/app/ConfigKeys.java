2e2296807e0c141ca53d282c97804d17bd12e795
Ykamh <1084168343@qq.com>
2018-02-02 22:49:03 +0800

    拦截器功能设计与实现(模拟请求)

---
+++ latte-core/src/main/java/com/myself/latte/app/ConfigKeys.java
@@ -0,0 +1,14 @@
+package com.myself.latte.app;
+
+/**
+ * Created by Administrator on 2018/1/15.
+ */
+
+public enum ConfigKeys {
+    API_HOST,
+    APPLICATION_CONTEXT,
+    CONFIG_READY,
+    ICON,
+    LOADER_DELAYED,
+    INTERCEPTOR
+}
