2ae4ef578b0d7919d26879b6f4c1e3b08902b5ae
Ykamh <1084168343@qq.com>
2018-03-03 16:46:08 +0800

    创建主页UI

--- latte-ec/src/main/java/com/myself/latte/ec/mian/index/IndexDelegate.java
+++ latte-ec/src/main/java/com/myself/latte/ec/mian/index/IndexDelegate.java
@@ -2,16 +2,39 @@ package com.myself.latte.ec.mian.index;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.AppCompatEditText;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.joanzapata.iconify.widget.IconTextView;
 import com.myself.latte.delegates.bottom.BottomItemDelegate;
 import com.myself.latte.ec.R;
+import com.myself.latte.ec.R2;
+
+import butterknife.BindView;
 
 /**
  * Created by Kamh on 2018/3/1.
  */
 
 public class IndexDelegate extends BottomItemDelegate {
+
+    @BindView(R2.id.rv_index)
+    RecyclerView mRecyclerView = null;
+    @BindView(R2.id.srl_index)
+    SwipeRefreshLayout mRefreshLayout = null;
+    @BindView(R2.id.tb_index)
+    Toolbar mToolbar = null;
+    @BindView(R2.id.icon_index_scan)
+    IconTextView mIconScan = null;
+    @BindView(R2.id.icon_index_message)
+    IconTextView mIconMessage = null;
+    @BindView(R2.id.et_search_view)
+    AppCompatEditText mSearchView = null;
+
+
     @Override
     public Object setLayout() {
         return R.layout.delegate_index;
