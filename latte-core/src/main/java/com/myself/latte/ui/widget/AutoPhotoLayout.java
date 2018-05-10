e45df693cf0970da3fd6ba341cf556befae23806
Ykamh <1084168343@qq.com>
2018-05-10 22:51:59 +0800

    仿微信自动多图展示及删除控件-3

--- latte-core/src/main/java/com/myself/latte/ui/widget/AutoPhotoLayout.java
+++ latte-core/src/main/java/com/myself/latte/ui/widget/AutoPhotoLayout.java
@@ -33,7 +33,7 @@ public class AutoPhotoLayout extends LinearLayoutCompat{
     private AppCompatImageView mTarget = null;
     private int mImageMargin = 0;
     private LatteDelegate mDelegate = null;
-    private List<View> mViewList = null;
+    private List<View> mLineViews = null;
     private AlertDialog mTargetDialog = null;
     private static final String ICON_TEXT = "{fa-plus}";
     private float mIconSize = 0;
@@ -43,6 +43,7 @@ public class AutoPhotoLayout extends LinearLayoutCompat{
 
     //防止多次的测量和布局过程
     private boolean mIsOnceInitOnMeasure = false;
+    private boolean mHasInitOnLayou = false;
 
     public AutoPhotoLayout(Context context) {
         this(context, null);
@@ -117,6 +118,77 @@ public class AutoPhotoLayout extends LinearLayoutCompat{
             lp = new LayoutParams(imagesSideLen, imagesSideLen);
             mIsOnceInitOnMeasure = true;
         }
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        ALL_VIEWS.clear();
+        LINE_HEIGHT.clear();
+        //获取当前ViewGroup的宽度
+        final int width = getWidth();
+        int lineWidth = 0;
+        int lineHeight = 0;
+
+        if (!mHasInitOnLayou){
+            mLineViews = new ArrayList<>();
+            mHasInitOnLayou = true;
+        }
+        final int cCount = getChildCount();
+        for (int i = 0; i< cCount; i++){
+            final View child = getChildAt(i);
+            final MarginLayoutParams lp = (MarginLayoutParams) child.getLayoutParams();
+            final int childWidth = child.getMeasuredWidth();
+            final int chidlHeight = child.getMeasuredHeight();
+            //如果需要换行
+            if (childWidth + lineWidth + lp.leftMargin + lp.rightMargin > width-getPaddingRight()-getPaddingLeft()){
+                //纪录lineHeight
+                LINE_HEIGHT.add(lineHeight);
+                //纪录当前的一行的Views
+                ALL_VIEWS.add(mLineViews);
+                //重置宽和高
+                lineWidth = 0;
+                lineHeight = chidlHeight+lp.topMargin+lp.bottomMargin;
+                //重置View集合
+                mLineViews.clear();
+            }
+            lineWidth+=childWidth+lp.leftMargin+lp.rightMargin;
+            lineHeight = Math.max(lineHeight, lineHeight+lp.topMargin+lp.bottomMargin);
+            mLineViews.add(child);
+        }
+        //处理最后一行
+        LINE_HEIGHT.add(lineHeight);
+        ALL_VIEWS.add(mLineViews);
+        //设置子View位置
+        int left = getPaddingLeft();
+        int top = getPaddingTop();
+        //行数
+        final int lineNum = ALL_VIEWS.size();
+        for (int i = 0; i < lineNum; i++){
+            //当前行的所有View
+            mLineViews = ALL_VIEWS.get(i);
+            lineHeight = LINE_HEIGHT.get(i);
+            final int size = mLineViews.size();
+            for (int j = 0; j < size; j++){
+                final View child = mLineViews.get(i);
+                //判断child的状态
+                if (child.getVisibility() == GONE){
+                    continue;
+                }
+                final MarginLayoutParams lp = (MarginLayoutParams) child.getLayoutParams();
+                //设置子View的边距
+                final int lc = left+lp.leftMargin;
+                final int tc = top+lp.topMargin;
+                final int rc = lc + child.getMeasuredWidth() - mImageMargin;
+                final int bc = tc + child.getMeasuredHeight();
+                //为子View进行布局
+                child.layout(lc, tc, rc, bc);
+                left+=child.getMeasuredWidth()+lp.leftMargin+lp.rightMargin;
+            }
+            left = getPaddingLeft();
+            top += lineHeight;
+        }
+        mIconAdd.setLayoutParams(lp);
+        mHasInitOnLayou = false;
     }
 
     private void initAddIcon(){
